433d0ade584a4366448e499e63616063715dd049
Sven H <sven.hjol@protonmail.com>
2024-06-17 22:38:25 +0000

    Bump versions

--- config.example.php
+++ config.example.php
@@ -3,9 +3,9 @@
 // Define this for the site it's running on.
 const BASE_URL = 'http://localhost:8000';
 
-const CHARM_VERSION = '1.21-7.0.20';
+const CHARM_VERSION = '1.21-7.0.23';
 const CHARMONIUM_VERSION = '1.21-7.0.0';
-const STRANGE_VERSION = '1.21-7.0.9';
+const STRANGE_VERSION = '1.21-7.0.11';
 
 const CHARM_FEATURES_URL = BASE_URL . '/charm-features.php';
 const CHARMONIUM_FEATURES_URL = BASE_URL . '/charmonium-features.php';
