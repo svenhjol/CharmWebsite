938fe6372caafc5069215b0fa6680157f44bc7e7
Sven H <sven.hjol@protonmail.com>
2024-06-18 22:58:00 +0000

    Bump to .25

--- config.example.php
+++ config.example.php
@@ -3,7 +3,7 @@
 // Define this for the site it's running on.
 const BASE_URL = 'http://localhost:8000';
 
-const CHARM_VERSION = '1.21-7.0.24';
+const CHARM_VERSION = '1.21-7.0.25';
 const CHARMONIUM_VERSION = '1.21-7.0.0';
 const STRANGE_VERSION = '1.21-7.0.12';
 
